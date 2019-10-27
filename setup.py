eda871f6312de867136931b7f645d8992651654c
Jaan Tollander de Balsch <jaan.tollander@gmail.com>
2019-10-27 19:04:28 +0200

    Updated author and description

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     packages=['cell_lists', 'cell_lists.tests'],
     url='https://github.com/jaantollander/cell_lists',
     license='MIT',
-    author='jaan',
+    author='Jaan Tollander de Balsch',
     author_email='jaan.tollander@gmail.com',
     description='Cell lists is an algorithm for computing fast fixed-radius '
-                'nearest neighbors search.'
+                'near neighbors search.'
 )
